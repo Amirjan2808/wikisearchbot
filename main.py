671525f9926141e93319b507e751ab66a36e2da8
Amirbek <olimovamirbek55@gmail.com>
2022-01-30 20:33:38 +0500

    settings and main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print(3+7)
+from telegram.ext import Updater, Dispatcher, CommandHandler, CallbackContext
+from telegram.update import Update
+import settings
+
+updater = Updater(token=settings.TELEGRAM_TOKEN)
+
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text("Salom!")
+
+dispatcher: Dispatcher = updater.dispatcher
+dispatcher.add_handler(CommandHandler('start', start))
+
+if __name__ == '__main__':
+    updater.start_polling()
+    updater.idle()
